00c9c74793505785be820ba126d81a85918d21b1
mario filho <mariosantofilho@gmail.com>
2019-06-03 19:10:33 -0300

    Desafio básico aula 014

--- js/description-product.js
+++ js/description-product.js
@@ -1,6 +1,20 @@
-const $addButonCart = window.document.querySelector(".action.-last");
-$addButonCart.addEventListener("click", handleClickCart);
+const $heart = window.document.querySelector(".-heart");
+$heart.addEventListener("click", handleClickActive);
+function handleClickActive() {
+  $heart.classList.toggle("-active");
+}
+
+/*if ($heart.classList.contains("-active")) {
+    console.log("Desative o like");
+    $heart.classList.remove("-active");
+  } else {
+    console.log("Ative o like");
+    $heart.classList.add("-active");
+  }*/
+
+const $buttonCartSecond = window.document.querySelector(".-second");
+$buttonCartSecond.addEventListener("click", handleClickCart);
 
 function handleClickCart() {
-  $addButonCart.innerHTML = "Cesta-->(10)";
+  console.log("Produto Adcionado");
 }
