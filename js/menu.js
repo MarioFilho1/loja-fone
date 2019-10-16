85d537e7e8bcf98d6a44d4e02d238b22923fa675
mario filho <mariosantofilho@gmail.com>
2019-10-16 09:30:13 -0300

    Desafio aula 019

--- js/menu.js
+++
@@ -1,8 +0,0 @@
-const $menu = document.querySelector(".menu");
-const $content = $menu.querySelector(".content");
-$menu.addEventListener("click", handleClickMenu);
-
-function handleClickMenu() {
-  $menu.classList.toggle("-active");
-  $content.classList.toggle("-active");
-}
